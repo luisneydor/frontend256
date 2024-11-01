76e2619d5206936a52aaf045a7fc80dc9d6810e3
Luis Sneyder <luisneydor@gmail.com>
2024-11-01 16:57:25 -0500

    Se agregan otras funciopnes del CRUD

--- js/productos.js
+++ js/productos.js
@@ -19,7 +19,7 @@
                 
             
             <td> 
-            <button type = "button" class = "btn btn-primary" onclick = 'window.location ="/formProductos.html ? id = ${element._id}"'> Editar</button>
+            <button type = "button" class = "btn btn-primary" onclick = 'window.location ="/formProductos.html?id=${element._id}"'> Editar</button>
             <button type = "button" class = "btn btn-danger" onclick = 'deleteProducto("${element._id}")'> Eliminar</button>
 
             </td>
@@ -36,5 +36,69 @@
         let request = sendRequest( 'productos/' +id, 'DELETE', '');
         request.onload = function() {
         mostrarProductos();
+        }
+     }
+
+     ///////////////
+
+     function guardarProductos(){
+        let ref = document.getElementById('referencia-r').value
+        let nom = document.getElementById('nombre-n').value
+        let pro = document.getElementById('proveedor-p').value
+        let cat = document.getElementById('categoria-c').value
+        let med = document.getElementById('medida-m').value
+       
+
+        let data = {'referencia':ref, 'nombre':nom, 'proveedor':pro,  'categoria':cat, 'medida':med,}
+        let request = sendRequest('productos/', 'POST', data)
+        request.onload = function(){
+        window.location = 'productos.html';
+       }
+        request.onerror = function() {
+           alert ('Error al guardar los datos')
+        }
+     }
+
+    
+     function cargarDatos(id) {
+        let request = sendRequest('productos/'+id, 'GET', '');
+        let ref = document.getElementById('referencia-r')
+        let nom = document.getElementById('nombre-n')
+        let pro = document.getElementById('proveedor-p')
+        let cat = document.getElementById('categoria-c')
+        let med = document.getElementById('medida-m')
+
+
+        request.onload = function() {
+            let data = request.response;
+            ref.value = data.referencia
+            nom.value = data.nombre
+            pro.value = data.proveedor
+            cat.value = data.categoria
+            med.value = data.medida
+            console.log(data)
+
+        }
+        request.onerror = function(){
+            alert("Error al guarda datos")
+        }
+    }
+function modificarProductos(id){
+       
+        let ref = document.getElementById('referencia-r').value
+        let nom = document.getElementById('nombre-n').value
+        let pro = document.getElementById('proveedor-p').value
+        let cat = document.getElementById('categoria-c').value
+        let med = document.getElementById('medida-m').value
+
+
+        let data = {'referencia':ref, 'nombre':nom, 'proveedor':pro,  'categoria':cat, 'medida':med,}
+        let request = sendRequest('productos/'+id, 'PUT', data)
+        console.log(request);
+        request.onload = function(){
+            window.location = 'productos.html';
+       }
+        request.onerror = function() {
+           alert ('Error al guardar los datos')
         }
      }
